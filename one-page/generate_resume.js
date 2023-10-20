d1701f961f6146f1569ea7a3a7d047bde6b101d6
Anthony Pipia <anthony.pipia@carvana.com>
2023-10-20 12:00:25 -0700

    updates

--- one-page/generate_resume.js
+++ one-page/generate_resume.js
@@ -1,5 +1,5 @@
 'use strict';
-var resume = require('./resume.json');
+var resume = require('../resume.json');
 var theme = require('jsonresume-theme-onepage');
 const fs = require('fs');
 fs.writeFile("./index.html", theme.render(resume), err => {if (err) { console.error(err);}});
