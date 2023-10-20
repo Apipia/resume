d93142927337f32672a9731348d4eaa50b2c7182
Anthony Pipia <anthony.pipia@carvana.com>
2023-10-20 12:27:00 -0700

    updated resume's and improved script for one-page version

--- generate_resume.js
+++ generate_resume.js
@@ -1,5 +1,13 @@
 'use strict';
 var resume = require('./resume.json');
 var theme = require('jsonresume-theme-eloquent-mod');
+var onePage = require('jsonresume-theme-onepage');
 const fs = require('fs');
 fs.writeFile("./index.html", theme.render(resume), err => {if (err) { console.error(err);}});
+
+resume.work = resume.work.map(obj => {
+	delete obj.summary;
+	return obj;
+});
+
+fs.writeFile("./one-page/index.html", onePage.render(resume), err => {if (err) { console.error(err);}});
--- one-page/generate_resume.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var resume = require('../resume.json');
-var theme = require('jsonresume-theme-onepage');
-const fs = require('fs');
-fs.writeFile("./index.html", theme.render(resume), err => {if (err) { console.error(err);}});
