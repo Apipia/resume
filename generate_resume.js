0a49ab0e89a2c828c77d71cf03b4a1dc04e20859
Anthony Pipia <anthony.pipia@carvana.com>
2023-10-20 11:20:59 -0700

    updated generate script to use index.html instead of resume.html

--- generate_resume.js
+++ generate_resume.js
@@ -2,4 +2,4 @@
 var resume = require('./resume.json');
 var theme = require('jsonresume-theme-eloquent-mod');
 const fs = require('fs');
-fs.writeFile("./resume.html", theme.render(resume), err => {if (err) { console.error(err);}});
+fs.writeFile("./index.html", theme.render(resume), err => {if (err) { console.error(err);}});
